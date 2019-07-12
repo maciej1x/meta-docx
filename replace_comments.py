6092f585623fb775f20327ea433acd106527b4b1
Maciej Ulaszewski <51076719+ulaszewskim@users.noreply.github.com>
2019-07-12 14:25:55 +0200

    Update and rename ReplaceComments.py to replace_comments.py

--- replace_comments.py
+++ replace_comments.py
@@ -8,15 +8,15 @@ github: https://github.com/ulaszewskim
 """
 
 import os
-from CreateNewDocx import CreateNewDocx
-from CreateTempZipDir import CreateTempZipDir
+from create_new_docx import create_new_docx
+from create_temp_zip_dir import create_temp_zip_dir
 from shutil import rmtree
 
 #==========
 #Replace comments in entry docx file and save to other docx file
 #==========
-def ReplaceComments(coms_to_change, newfile, docxfile):
-    CreateTempZipDir(os.path.split(docxfile)[1], os.path.split(docxfile)[0])
+def replace_comments(coms_to_change, newfile, docxfile):
+    create_temp_zip_dir(os.path.split(docxfile)[1], os.path.split(docxfile)[0])
     
     xmlcom = open(os.path.join(os.path.split(docxfile)[0],'temp_meta_docx/word/comments.xml'), 'r').read()
     for i in range(len(coms_to_change)):
@@ -29,6 +29,6 @@ def ReplaceComments(coms_to_change, newfile, docxfile):
     if os.path.exists(newfile+'.zip'):
         os.remove(newfile+'.zip')
     
-    CreateNewDocx(os.path.join(os.path.split(docxfile)[0],'temp_meta_docx/'), newfile)
+    create_new_docx(os.path.join(os.path.split(docxfile)[0],'temp_meta_docx/'), newfile)
     rmtree(os.path.join(os.path.split(docxfile)[0],'temp_meta_docx/'))
     
