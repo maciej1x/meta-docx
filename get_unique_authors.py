df3c6c4755516aef32304bcc18fdbfbb61138630
Maciej Ulaszewski <51076719+ulaszewskim@users.noreply.github.com>
2019-07-25 11:40:47 +0200

    Update get_unique_authors.py

--- get_unique_authors.py
+++ get_unique_authors.py
@@ -1,22 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-
 @author: Maciej Ulaszewski
 mail: ulaszewski.maciej@gmail.com
 github: https://github.com/ulaszewskim
 
 """
 
-#==========
-#get all unique authors names with initials of comments
-#returns list of tuples, for example: ('Author Name','AN')
-#==========
+
 def get_unique_authors(comments):
+    """
+    get all unique authors names with initials of comments
+    returns list of tuples
+    for example: ('Author Name','AN')
+    """
     ns = {'w':'http://schemas.openxmlformats.org/wordprocessingml/2006/main'}
-    authors=[]
+    authors = []
     for comment in comments:
-        author=str(comment.xpath('@w:author',namespaces=ns))[2:-2]
-        initials = str(comment.xpath('@w:initials',namespaces=ns))[2:-2]
+        author = str(comment.xpath('@w:author', namespaces=ns))[2:-2]
+        initials = str(comment.xpath('@w:initials', namespaces=ns))[2:-2]
         authors.append((author, initials))
     authors = list(dict.fromkeys(authors)) #remove duplicates
     return authors
