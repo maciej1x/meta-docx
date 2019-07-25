237be5fd3fa056cbdbb482253b87b4397afd42b7
Maciej Ulaszewski <51076719+ulaszewskim@users.noreply.github.com>
2019-07-25 11:23:13 +0200

    Update create_new_docx.py

--- create_new_docx.py
+++ create_new_docx.py
@@ -10,10 +10,9 @@ github: https://github.com/ulaszewskim
 import os
 import zipfile
 
-#==========
-#Create docx from directory
-#==========
+
 def create_new_docx(directory, docxname):
+    """Create docx from directory"""
     zip_file = zipfile.ZipFile(docxname, 'w', zipfile.ZIP_DEFLATED)
     rootdir = os.path.basename(directory)
     for dirpath, dirnames, filenames in os.walk(directory):
