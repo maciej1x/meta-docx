865dbaa73f033dea78ddd2f664896f7b54797bb4
Maciej Ulaszewski <51076719+ulaszewskim@users.noreply.github.com>
2019-07-25 11:35:18 +0200

    Update create_temp_zip_dir.py

--- create_temp_zip_dir.py
+++ create_temp_zip_dir.py
@@ -11,18 +11,15 @@ import os
 import zipfile
 from shutil import copy2, rmtree
 
-#==========
-#create docx with new comments
-#==========
 
 def create_temp_zip_dir(docxfile, path_to_docxfile):
-    if os.path.exists(os.path.join(path_to_docxfile,'temp_meta_docx')):
-        rmtree(os.path.join(path_to_docxfile,'temp_meta_docx'))
-    os.mkdir(os.path.join(path_to_docxfile,'temp_meta_docx'))
-    
-    zip_file_path = os.path.join(path_to_docxfile,'temp_meta_docx',docxfile[:-4]+'zip')
-    copy2(os.path.join(path_to_docxfile,docxfile), zip_file_path)
+    """create docx with new comments"""
+    if os.path.exists(os.path.join(path_to_docxfile, 'temp_meta_docx')):
+        rmtree(os.path.join(path_to_docxfile, 'temp_meta_docx'))
+    os.mkdir(os.path.join(path_to_docxfile, 'temp_meta_docx'))
+    zip_file_path = os.path.join(path_to_docxfile, 'temp_meta_docx', docxfile[:-4]+'zip')
+    copy2(os.path.join(path_to_docxfile, docxfile), zip_file_path)
     zip_file = zipfile.ZipFile(zip_file_path, 'r')
-    zip_file.extractall(os.path.join(path_to_docxfile,'temp_meta_docx/'))
+    zip_file.extractall(os.path.join(path_to_docxfile, 'temp_meta_docx/'))
     zip_file.close()
     os.remove(zip_file_path)
